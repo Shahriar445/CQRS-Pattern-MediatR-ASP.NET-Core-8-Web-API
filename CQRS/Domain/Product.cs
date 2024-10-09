ab84b25f3d1fbebe0b15e6a7ad9c3fc71b12d473
Shahriar Haque <91280914+Shahriar445@users.noreply.github.com>
2024-10-09 17:50:35 +0600

    Add project files.

---
+++ CQRS/Domain/Product.cs
@@ -0,0 +1,23 @@
+﻿namespace CQRS.Domain;
+
+public class Product
+{
+    public Guid Id { get; set; }
+    public string? Name { get; set; }
+    public string? Description {get;set;}
+    public decimal? Price { get; set; }
+
+
+
+        //ParameterLess constructor for ef core 
+        private Product() { }
+    public Product(string name,string description, decimal price)
+    {
+        Id =Guid.NewGuid();
+        Name =name;
+        Description =description;   
+        Price =price;
+    }
+
+}
+
