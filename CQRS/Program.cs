712cc64b1e8c9d0cb784faef2ba7e65963a80fe7
Shahriar Haque <91280914+Shahriar445@users.noreply.github.com>
2024-10-09 17:59:39 +0600

    upto mediatR

--- CQRS/Program.cs
+++ CQRS/Program.cs
@@ -1,3 +1,5 @@
+using CQRS.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +9,8 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+builder.Services.AddDbContext<AppDbContext>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
